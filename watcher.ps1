2b5a47e5a36f9da048c47b085bb7cf3db14e612f
Eedrah <eedrah@users.noreply.github.com>
2015-06-27 20:06:21 +1200

    write watcher script

--- watcher.ps1
+++ watcher.ps1
@@ -1,1 +1,13 @@
 # This watches the master branch for changes
+
+function watch {
+    $sha = git rev-parse master
+
+    while($true){
+        if(git rev-parse master != $sha){
+            $sha = git rev-parse master
+            git merge master
+        }
+        sleep 1
+    }
+}
